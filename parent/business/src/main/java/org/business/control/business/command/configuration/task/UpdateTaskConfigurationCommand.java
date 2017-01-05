5963991fb47f7a2c3da4a08fedb8260238e98629
Mickaël PONSOLLE <mickael.ponsolle@gmail.com>
2017-01-05 21:09:04 +0100

    Update Task Configuration command

---
+++ parent/business/src/main/java/org/business/control/business/command/configuration/task/UpdateTaskConfigurationCommand.java
@@ -0,0 +1,21 @@
+package org.business.control.business.command.configuration.task;
+
+import org.business.control.business.command.BusinessCommand;
+
+public class UpdateTaskConfigurationCommand implements BusinessCommand {
+    private final Long id;
+    private final String title;
+
+    public UpdateTaskConfigurationCommand(Long id, String title) {
+	this.id = id;
+	this.title = title;
+    }
+
+    public Long getId() {
+	return id;
+    }
+
+    public String getLibelle() {
+	return title;
+    }
+}
